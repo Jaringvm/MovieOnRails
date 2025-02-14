92c10edeef47ac25ce7303f121632174cfb2df16
Jaringvm <74595376+Jaringvm@users.noreply.github.com>
2025-02-14 14:39:09 +0100

    added all pages connected to the home page
    upload, review, about, overview and of course the homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   root "pages#home"
+  get :upload, to: "pages#upload"
+  get :review, to: "pages#review"
+  get :about, to: "pages#about"
+  get :overview, to: "pages#overview"
 end
