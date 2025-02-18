3868dc8aecfa972dd6443aa64ef9e99f2aca76c8
Jaringvm <74595376+Jaringvm@users.noreply.github.com>
2025-02-18 12:07:41 +0100

    code cleanup

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -1,20 +1,12 @@
 class UploadsController < ApplicationController
   def new
     if params[:submitted] == "true"
-
-      @movie_headers = Movie.column_names - %W[id]
-      @location_headers = Location.column_names - %W[id]
-      @actor_headers = Actor.column_names - %W[id]
-
-      @movie_headers.map { |header| [header, header] }.to_h
-      @location_headers.map { |header| [header, header] }.to_h
-      @actor_headers.map { |header| [header, header] }.to_h
-
       @arr = {
-        "movies": @movie_headers,
-        "locations": @location_headers,
-        "actors": @actor_headers
+        "movies": (Movie.column_names - %W[id]).map { |header| [header, header] }.to_h,
+        "locations": (Location.column_names - %W[id]).map { |header| [header, header] }.to_h,
+        "actors": (Location.column_names - %W[id]).map { |header| [header, header] }.to_h,
       }
+
 
       if params[:file_headers].length == 1
         @uploaded_headers = params[:file_headers][0].split(";")
@@ -25,7 +17,6 @@ class UploadsController < ApplicationController
   end
 
   def create
-    puts params
     movies = params["movies"]
     locations = params["locations"]
     actors = params["actors"]
@@ -49,6 +40,8 @@ class UploadsController < ApplicationController
       movie.actors << actor unless movie.actors.include?(actor)
       movie.locations << location unless movie.locations.include?(location)
     end
+
+    redirect_to new_upload_path
   end
 
   def import_file
@@ -58,13 +51,10 @@ class UploadsController < ApplicationController
 
       $csv = CSV.read(file, headers: true)
       headers = $csv.headers
-      puts headers
       redirect_to new_upload_path(submitted: true, file_headers: headers)
     else
       flash[:error] = "Only files with .csv extension are allowed!"
       redirect_to new_upload_path
     end
   end
-
-
 end
