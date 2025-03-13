e20e455fcb3076cd92a0deeb088aaab546f91b7b
Jaringvm <74595376+Jaringvm@users.noreply.github.com>
2025-03-13 11:24:18 +0100

    Documented UploadsController

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -1,14 +1,21 @@
 class UploadsController < ApplicationController
+  # Parses the database structure to a hashes with column names as keys and empty values.
+  # @param [Boolean] :submitted Signals when the user imports a csv file.
+  # @param [String] :dataType Shows if the user is uploading movies or reviews.
+  # @param [Array] :file_headers Column headers extracted from imported csv file.
   def new
+    # Ensures a file was uploaded.
     if params[:submitted] == "true"
       if params[:dataType] == "Movies"
-        @arr = {
+        # Parses the database structure into a nested hash.
+        @db_structure = {
           "movies": (Movie.column_names - %w[id]).map { |header| [header, header] }.to_h,
           "locations": (Location.column_names - %w[id]).map { |header| [header, header] }.to_h,
           "actors": (Actor.column_names - %w[id]).map { |header| [header, header] }.to_h,
         }
       elsif params[:dataType] == "Reviews"
-        @arr = {
+        # Parses the database structure into a nested hash.
+        @db_structure = {
           "reviews": (Review.column_names - %w[id]).map { |header| [header, header] }.to_h,
           "users": (User.column_names - %w[id]).map { |header| [header, header] }.to_h,
         }
@@ -16,11 +23,17 @@ class UploadsController < ApplicationController
         flash[:error] = "Please pick between the provided data types."
         redirect_to new_upload_path
       end
-
+      # Splits the uploaded_headers if the headers are split by ';' instead of ','.
       @uploaded_headers = params[:file_headers].is_a?(Array) ? params[:file_headers] : params[:file_headers][0].split(";")
     end
   end
 
+  # Inserts the uploaded csv file to the database based on the by the user mapped headers.
+  # @param [Hash{String => String}] 'reviews' Holds the mapped review headers.
+  # @param [Hash{String => String}] 'users' Holds the mapped user headers.
+  # @param [Hash{String => String}] 'movies' Holds the mapped movie headers.
+  # @param [Hash{String => String}] 'locations' Holds the mapped location headers.
+  # @param [Hash{String => String}] 'actors' Holds the mapped actor headers.
   def create
     if params["reviews"]
       reviews = params["reviews"]
@@ -30,11 +43,11 @@ class UploadsController < ApplicationController
         movie = Movie.find_or_create_by!(name: row[reviews["movie_id"]])
         user = User.find_or_create_by!(name: row[users["name"]])
 
-        review = Review.create!(
+        Review.create!(
           rating: row[reviews["rating"]],
           content: row[reviews["content"]],
           movie: movie,
-          user: user  # Fix: Assign user directly
+          user: user
         )
       end
 
@@ -53,6 +66,7 @@ class UploadsController < ApplicationController
           m.director = row[movies["director"]]
         end
 
+        # Prevents duplicate relations.
         movie.actors << actor unless movie.actors.include?(actor)
         movie.locations << location unless movie.locations.include?(location)
       end
@@ -65,14 +79,20 @@ class UploadsController < ApplicationController
     redirect_to root_path
   end
 
+  # Checks if the file is the correct extension and reads the headers of the csv file.
+  # @param [File] :file By the user uploaded file.
+  # @param [String] :dataType shows if the user is uploading movies or reviews.
   def import_file
     require 'csv'
+
+    # Checks if the imported file is the correct extension
     if params[:file] && File.extname(params[:file].path) == '.csv'
       file = params[:file].path
 
       $csv = CSV.read(file, headers: true)
       headers = $csv.headers
       data_type = params[:data_type]
+
       redirect_to new_upload_path(submitted: true, file_headers: headers, dataType: data_type)
     else
       flash[:error] = "Only .csv files are allowed!"
